af4ba0c297418915083bc1775096ec51cd1900ee
anusree-ux <anusreemanojmk2004@gmail.com>
2026-07-09 07:16:50 +0000

    Multi-stage build

--- backend/generate_logs.py
+++ backend/generate_logs.py
@@ -25,7 +25,7 @@ while True:
     }
 
     req = urllib.request.Request(
-        'http://127.0.0.1:5000/api/logs',
+        'http://localhost/api/logs',
         data=json.dumps(log_data).encode('utf-8'),
         headers={'Content-Type': 'application/json'}
     )
